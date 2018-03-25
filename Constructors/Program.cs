09be48b547651e0e27381fbd6398aa2edc53174c
Ivan Cedeño <icedeno@gmail.com>
2018-03-25 11:51:59 -0400

    Constructors Demo

--- Constructors/Program.cs
+++ Constructors/Program.cs
@@ -10,6 +10,20 @@ namespace Constructors
     {
         static void Main(string[] args)
         {
+            var emp = new Employee(5, 101);
+
+            Console.WriteLine($"emp years of service: {emp.YearsOfService}");
+            Console.WriteLine($"emp Office: {emp.Office}");
+
+            var emp2 = new Employee
+            {
+                YearsOfService = 15,
+                Office = 102
+            };
+
+            Console.WriteLine($"emp2 years of service: {emp2.YearsOfService}");
+            Console.WriteLine($"emp2 Office: {emp2.Office}");
+
         }
     }
 
